a8bb4e0478ed04f48e6e865865d37b0ce82658db
E41162071/ArifRahmatA <arief3idiot@gmail.com>
2018-06-04 15:09:51 +0700

    no message

--- application/views/pengajuan/pengajuanview.php
+++ application/views/pengajuan/pengajuanview.php
@@ -1,65 +1,108 @@
-<html>
-<head>
-    <title>
-    pengajuan
-    </title>
-    <style>     
-                
-ul {
-    text-align: center;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    border-bottom-color: #2ebbde;
-   
-}
- /* untuk membuat bewarna memanjang */                            
-#header ul li a{
-color: Black;
-text-decoration:none; 
-padding: 0px 60px;
-/*meberi jarak antar
-teks*/
-font:15pt comic sans;/*mengatur format teks dgn
-size 15pt dan jns font cambria*/
-font-weight:bold;
-}
-        
-#header ul li a:hover{
-color:black;
-background-color:#94a6af;
-} /* ketika mouse menyentuh button berubah */
-    
-#header ul li{
-display:inline; /* membuat tampilan list
-sejajar ke samping */
-padding:7px 9px 7px 9px;
-background-color:#eef1f4;
-}
-
-            
-}
-            </style>
-    </head>
-    
-    <br><br>
-    <div id="header">
-        <ul>
-        
-        <li><a  href = "http://localhost/Peminjaman/menu/index" rel='stylesheet' type='text/css' name="ajukan">Diajukan</a></li>
-	
-		<li><a href = "http://localhost/Peminjaman/menu/disetujui"  rel='stylesheet' type='text/css'>Disetujui</a></li>
-		
-		<li><a href = "http://localhost/Peminjaman/menu/ditolak"  rel='stylesheet' type='text/css'>Ditolak</a></li>
-		
-		<li><a  href="http://localhost/Peminjaman/menu/semua"  rel='stylesheet' type='text/css'>Semua</a></li>
-        </ul>
-    </div>
-       <body style="background-color: aliceblue">      
-
-    
-    </body>
-       
-   
+<html lang="en">
+<body>        
+<div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Admin</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="glyphicon glyphicon-list-alt fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                   <div>Diajukan</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="http://localhost/Peminjaman/menu/index">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="glyphicon glyphicon-ok fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                  
+                                    <div>Disetujui</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="http://localhost/Peminjaman/menu/disetujui">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="glyphicon glyphicon-remove fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    
+                                    <div>Ditolak</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="http://localhost/Peminjaman/menu/ditolak">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="glyphicon glyphicon-edit fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    
+                                    <div>Semua</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="http://localhost/Peminjaman/menu/semua">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            </i>Diajukan
+						</div>
+						
+                        
+        </body>
 </html>
